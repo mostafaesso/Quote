305313262ed43932987a1334445bbf5fc02cca37
Mostafa Ali <73172565+mostafaesso@users.noreply.github.com>
2026-07-26 11:47:27 -0500

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import QuoteGenerator from './QuoteGenerator';
+
+export default function App() {
+  return (
+    <div className='min-h-screen bg-gradient-to-br from-slate-50 to-slate-100'>
+      <QuoteGenerator />
+    </div>
+  );
+}
